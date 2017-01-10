a9ce4aaaa890e48cb54deba2ae5531f9717c8cc1
510691 <510691@Student.Glu.Nl>
2017-01-10 10:47:19 +0100

    ...

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+namespace ARRockPaperScissor
+{
+    public class GameManager : NetworkBehaviour
+    {
+        private int m_MaxPlayers = 2;
+
+        public override void OnStartServer()
+        {
+            Network.maxConnections = m_MaxPlayers;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,29 +1,99 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 
-namespace AugmentedRPS
+namespace ARRockPaperScissor
 {
     public class PlayerController : NetworkBehaviour
     {
         [SerializeField]
-        private string m_Canvas;
+        private GameObject m_Canvas;
+        [SerializeField]
+        private GameObject m_EventSystem;
 
         [SerializeField]
-        private GameObject m_CanvasChild;
+        private GameObject[] m_CardsPreview = new GameObject[3];
+        [SerializeField]
+        private GameObject[] m_CardsPlaceable = new GameObject[3];
 
-        private void Start()
+        [SerializeField]
+        private NetworkIdentity m_NetworkIdentity;
+
+        private int m_CurrentCard = 0;
+
+        public override void OnStartLocalPlayer()
+        {
+            m_Canvas.SetActive(true);
+            m_EventSystem.SetActive(true);
+        }
+
+        private void ShowCard(int index)
         {
             if (!isLocalPlayer)
             {
                 return;
             }
 
-            GameObject GO = Instantiate(m_CanvasChild);
-            GO.transform.SetParent(GameObject.Find(m_Canvas).transform);
+            for (int i = 0; i < m_CardsPreview.Length; i++)
+            {
+                m_CardsPreview[i].SetActive(false);
+            }
 
-            RectTransform rectTransform = GO.GetComponent<RectTransform>();
-            rectTransform.anchoredPosition = new Vector2(0f, 0f);
-            rectTransform.localScale = new Vector3(1f, 1f, 1f);
+            m_CardsPreview[index].SetActive(true);
+        }
+
+        public void PreviousCard()
+        {
+            if (!isLocalPlayer)
+            {
+                return;
+            }
+
+            if (m_CurrentCard > 0)
+            {
+                m_CurrentCard -= 1;
+            }
+            else if (m_CurrentCard == 0)
+            {
+                m_CurrentCard = m_CardsPreview.Length - 1;
+            }
+
+            ShowCard(m_CurrentCard);
+        }
+
+        public void NextCard()
+        {
+            if (!isLocalPlayer)
+            {
+                return;
+            }
+
+            if (m_CurrentCard < m_CardsPreview.Length - 1)
+            {
+                m_CurrentCard += 1;
+            }
+            else if (m_CurrentCard == m_CardsPreview.Length - 1)
+            {
+                m_CurrentCard = 0;
+            }
+
+            ShowCard(m_CurrentCard);
+        }
+
+        [Command]
+        public void CmdPlaceCard()
+        {
+            if (m_NetworkIdentity.netId.ToString() == "1")
+            {
+                GameObject card = Instantiate(m_CardsPlaceable[m_CurrentCard], new Vector3(0f, 0.25f, -3f), Quaternion.identity);
+
+                NetworkServer.Spawn(card);
+            }
+            else if (m_NetworkIdentity.netId.ToString() == "2")
+            {
+                GameObject card = Instantiate(m_CardsPlaceable[m_CurrentCard], new Vector3(0f, 0.25f, 3f), Quaternion.identity);
+
+                NetworkServer.Spawn(card);
+            }
         }
     }
 }
