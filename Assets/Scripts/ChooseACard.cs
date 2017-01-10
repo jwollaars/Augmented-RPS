b828178a93a38d208d4b81540b9a0c8b5fb40a9e
jwollaars <jordywollaars@kpnmail.nl>
2017-01-10 10:40:35 +0100

    Revert "..."
    This reverts commit d02c64ee54f08d07892a1119614cdea05bc66c22.

---
+++ Assets/Scripts/ChooseACard.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace SeriousGame
+{
+    public class ChooseACard : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject[] m_CardsPlace = new GameObject[3];
+
+        [SerializeField]
+        private GameObject[] m_CardsPreview = new GameObject[3];
+
+        private int m_CurrentCard = 0;
+
+        private void ShowCard(int index)
+        {
+            for (int i = 0; i < m_CardsPreview.Length; i++)
+            {
+                m_CardsPreview[i].SetActive(false);
+            }
+
+            m_CardsPreview[index].SetActive(true);
+        }
+
+        public void PreviousCard()
+        {
+            if (m_CurrentCard > 0)
+            {
+                m_CurrentCard -= 1;
+            }
+            else if (m_CurrentCard == 0)
+            {
+                m_CurrentCard = 2;
+            }
+
+            ShowCard(m_CurrentCard);
+        }
+
+        public void NextCard()
+        {
+            if (m_CurrentCard < 2)
+            {
+                m_CurrentCard += 1;
+            }
+            else if (m_CurrentCard == 2)
+            {
+                m_CurrentCard = 0;
+            }
+
+            ShowCard(m_CurrentCard);
+        }
+
+        public void PlaceCard()
+        {
+            GameObject card = Instantiate(m_CardsPlace[m_CurrentCard], new Vector3(0f, 0.5f, 0f), Quaternion.identity);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,99 +1,29 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 
-namespace ARRockPaperScissor
+namespace AugmentedRPS
 {
     public class PlayerController : NetworkBehaviour
     {
         [SerializeField]
-        private GameObject m_Canvas;
-        [SerializeField]
-        private GameObject m_EventSystem;
+        private string m_Canvas;
 
         [SerializeField]
-        private GameObject[] m_CardsPreview = new GameObject[3];
-        [SerializeField]
-        private GameObject[] m_CardsPlaceable = new GameObject[3];
+        private GameObject m_CanvasChild;
 
-        [SerializeField]
-        private NetworkIdentity m_NetworkIdentity;
-
-        private int m_CurrentCard = 0;
-
-        public override void OnStartLocalPlayer()
-        {
-            m_Canvas.SetActive(true);
-            m_EventSystem.SetActive(true);
-        }
-
-        private void ShowCard(int index)
+        private void Start()
         {
             if (!isLocalPlayer)
             {
                 return;
             }
 
-            for (int i = 0; i < m_CardsPreview.Length; i++)
-            {
-                m_CardsPreview[i].SetActive(false);
-            }
+            GameObject GO = Instantiate(m_CanvasChild);
+            GO.transform.SetParent(GameObject.Find(m_Canvas).transform);
 
-            m_CardsPreview[index].SetActive(true);
-        }
-
-        public void PreviousCard()
-        {
-            if (!isLocalPlayer)
-            {
-                return;
-            }
-
-            if (m_CurrentCard > 0)
-            {
-                m_CurrentCard -= 1;
-            }
-            else if (m_CurrentCard == 0)
-            {
-                m_CurrentCard = m_CardsPreview.Length - 1;
-            }
-
-            ShowCard(m_CurrentCard);
-        }
-
-        public void NextCard()
-        {
-            if (!isLocalPlayer)
-            {
-                return;
-            }
-
-            if (m_CurrentCard < m_CardsPreview.Length - 1)
-            {
-                m_CurrentCard += 1;
-            }
-            else if (m_CurrentCard == m_CardsPreview.Length - 1)
-            {
-                m_CurrentCard = 0;
-            }
-
-            ShowCard(m_CurrentCard);
-        }
-
-        [Command]
-        public void CmdPlaceCard()
-        {
-            if (m_NetworkIdentity.netId.ToString() == "1")
-            {
-                GameObject card = Instantiate(m_CardsPlaceable[m_CurrentCard], new Vector3(0f, 0.25f, -3f), Quaternion.identity);
-
-                NetworkServer.Spawn(card);
-            }
-            else if (m_NetworkIdentity.netId.ToString() == "2")
-            {
-                GameObject card = Instantiate(m_CardsPlaceable[m_CurrentCard], new Vector3(0f, 0.25f, 3f), Quaternion.identity);
-
-                NetworkServer.Spawn(card);
-            }
+            RectTransform rectTransform = GO.GetComponent<RectTransform>();
+            rectTransform.anchoredPosition = new Vector2(0f, 0f);
+            rectTransform.localScale = new Vector3(1f, 1f, 1f);
         }
     }
 }
